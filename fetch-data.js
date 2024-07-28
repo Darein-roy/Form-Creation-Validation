a29a90a1cc41e2995a12baca7f3d41aacf77e14d
George Ouma <georgetieno@gmail.com>
2024-07-28 18:10:07 +0300

    new files

---
+++ fetch-data.js
@@ -0,0 +1,35 @@
+// Initializing the async function
+async function fetchUserData (){
+    
+    // defining the API URL
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    // selecting data container element
+    const dataContainer = document.getElementById('api-data');
+
+    // fetching the data 
+    try {
+        const response = await fetch('apiUrl');
+        const users = await response.json();
+
+        // clear the loading massage 
+        dataContainer.innerHTML = '';
+
+        // create and append userlist
+        const userList = document.createElement('ul');
+
+        users.array.forEach(user => {
+        const userName = document.createElement('li');
+        userName.textContent = 'user';
+        document.userList.appendChild(userName);
+        });
+        document.dataContainer.appendChild(userList);
+    } catch {
+        dataContainer.innerHTML = 'Failed to load user data.';
+
+    };   
+};
+
+addEventListener('DOMContentLoaded', function(){
+    fetchUserData();
+});
